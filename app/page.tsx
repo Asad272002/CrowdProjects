628e18fbd18fefecca6fea24f1f4ef2b913c5f5a
Asad272002 <asadnadeem6799@gmail.com>
2026-03-29 16:58:48 +0500

    Landing page compelete

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { HeroSection } from "@/components/landing-page/hero-section";
 import FeaturedProducts from "@/components/landing-page/featured-products";
+import RecentlyLauchedProducts from "@/components/landing-page/recently-launched-products";
 export default function Home() {
   return (
     <div>
     <HeroSection />
 
     <FeaturedProducts />
+    <RecentlyLauchedProducts />
 </div>
 );
 }
---
+++ components/landing-page/recently-launched-products.tsx
@@ -0,0 +1,30 @@
+import { CalendarIcon } from "lucide-react";
+import SectionHeader from "../common/section-header";
+import ProductCard from "../products/product-card";
+import EmptyState from "../common/empty-state";     
+
+export default function RecentlyLaunchedProducts() {
+const recentlyLaunchedProducts = []
+
+
+  return (
+    <section className="py-20">
+    <div className="wrapper space-y-12">
+    <SectionHeader 
+    title="Recently Launched Products" 
+    icon={CalendarIcon} 
+    description="Check out the latest products we've launched." />
+   
+    {recentlyLaunchedProducts.length > 0 ?(
+                <div 
+     className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {recentlyLaunchedProducts.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>):(
+            <EmptyState message="No Products launched recently." icon={CalendarIcon} />
+        )}
+     </div>
+    </section>
+  );
+}
